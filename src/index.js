508cdd27fe36a50f4c8f04a6bce7c007c5eb36d9
alanjames00 <alanjames6282@gmail.com>
2024-06-13 12:15:46 +0530

    add route for rayId sender

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono();
+
+app.get('/', async (c) => {
+	try {
+		const id = c.req.header('cf-ray');
+
+		return c.json({
+			rayId: id,
+		});
+	} catch (e) {
+		console.log(e);
+		return c.json(
+			{
+				err: e.message,
+			},
+			500,
+		);
+	}
+});
+
+export default app;
